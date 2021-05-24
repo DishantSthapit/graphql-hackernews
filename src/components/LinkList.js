efbde1545d4a2b5274909ef1f2297ee2c186d3ca
Dishant Sthapit <dishantsthapit@gmail.com>
2021-05-24 21:08:34 +1000

    Creating LinkList and Link component to query and mutate

---
+++ src/components/LinkList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "./Link";
+import { useQuery, gql } from "@apollo/client";
+
+const LinkList = () => {
+  const FEED_QUERY = gql`
+    {
+      feed {
+        id
+        links {
+          id
+          createdAt
+          url
+          description
+        }
+      }
+    }
+  `;
+
+
+  const { data, loading } = useQuery(FEED_QUERY);
+
+  console.log(data);
+
+  return loading ? (
+    <div>Loading</div>
+  ) : (
+    <div>
+      {data &&
+        data.feed.links.map((link) => <Link key={link.id} link={link} />)}
+    </div>
+  );
+};
+
+export default LinkList;
